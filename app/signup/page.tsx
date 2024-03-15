42deb57bd055e888eb79fc4fb13f19a1a9457cc0
加納大稀 <kanoudaiki@kanounoMacBook-Pro.local>
2024-03-15 12:34:20 +0900

    修正3

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import apiClient from "@/lib/apiClient";
-import { formSignUpSchema } from "@/lib/formSignUpSchema";
+import { formSignUpSchema } from "@/lib/formSchema";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import Head from "next/head";
